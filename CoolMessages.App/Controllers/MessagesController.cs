119884b66f246eb6a0e84a36d7e31a203eeeddfb
Luis Felipe <luisfelipekai@gmail.com>
2021-01-16 13:32:19 -0300

    Artigo parte 1.

---
+++ CoolMessages.App/Controllers/MessagesController.cs
@@ -0,0 +1,50 @@
+﻿using CoolMessages.App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace CoolMessages.App.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ConnectionFactory _factory;
+        private const string QUEUE_NAME = "messages";
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] MessageInputModel message)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var stringfiedMessage = JsonConvert.SerializeObject(message);
+                    var bytesMessage = Encoding.UTF8.GetBytes(stringfiedMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: bytesMessage);
+                }
+            }
+
+            return Accepted();
+        }
+    }
+}
---
+++ CoolMessages.App/Models/MessageInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoolMessages.App.Models
+{
+    public class MessageInputModel
+    {
+        public int FromId { get; set; }
+        public int ToId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
